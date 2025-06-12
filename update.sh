ada632aa17f5ebe49aaae8849d7149838e447f61
root <tanakamasayuki@gmail.com>
2025-06-12 10:47:07 +0900

    update

--- update.sh
+++ update.sh
@@ -8,13 +8,13 @@ git pull
 # https://www.wch-ic.com/products/CH32V208.html
 cd datasheet_en
 # https://www.wch-ic.com/downloads/CH32V203DS0_PDF.html
-curl -z CH32V203DS0.PDF -o CH32V203DS0.PDF https://www.wch-ic.com/downloads/file/354.html
+curl -z CH32V203DS0.PDF -o CH32V203DS0.PDF https://www.wch-ic.com/download/file?id=354
 # https://www.wch-ic.com/downloads/CH32V208DS0_PDF.html
-curl -z CH32V208DS0.PDF -o CH32V208DS0.PDF https://www.wch-ic.com/downloads/file/355.html
+curl -z CH32V208DS0.PDF -o CH32V208DS0.PDF https://www.wch-ic.com/download/file?id=355
 # https://www.wch-ic.com/downloads/CH32V20x_30xDS0_PDF.html
-curl -z CH32V20x_30xDS0.PDF -o CH32V20x_30xDS0.PDF https://www.wch-ic.com/downloads/file/325.html
+curl -z CH32V20x_30xDS0.PDF -o CH32V20x_30xDS0.PDF https://www.wch-ic.com/download/file?id=325
 # https://www.wch-ic.com/downloads/CH32FV2x_V3xRM_PDF.html
-curl -z CH32FV2x_V3xRM.PDF -o CH32FV2x_V3xRM.PDF https://www.wch-ic.com/downloads/file/324.html
+curl -z CH32FV2x_V3xRM.PDF -o CH32FV2x_V3xRM.PDF https://www.wch-ic.com/download/file?id=324
 cd ..
 
 # https://www.wch.cn/products/CH32V203.html
